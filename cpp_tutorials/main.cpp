4048d2f0c92480edfadb8b0db667456d5f1da8d2
Porcelain2017 <s.a.banniy@gmail.com>
2017-03-01 01:44:51 +0300

    основные типы данных

--- cpp_tutorials/main.cpp
+++ cpp_tutorials/main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int main()
 {
-	cout << "Hello world!" << endl;
+	//основные типы данных
+	int a = 1;			//целое число
+	float b = 0.0;		//с плавающей точкой
+	double c = 0.0;		//с плавающей точкой двойной точности
+	char d = 'a';		//символ 1 шт
+	bool e = true;		//логический
+	
+
 	return 0;
 }
 
